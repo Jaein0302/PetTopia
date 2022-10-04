427b59eba65cd9608d969c4a4af5454ec1ae57fc
Sijoon Choi <ahslxj1993@naver.com>
2022-10-04 17:22:13 +0900

    스케줄 데이터를 json 형식 문자열로 만들기 까지는 성공

--- PET/src/main/resources/static/sql/sch.sql
+++ PET/src/main/resources/static/sql/sch.sql
@@ -1,6 +1,10 @@
-create sch(
-sch_id varchar2(10) not null primary key,
-sch_name varchar2(100) not null references item(name) on delete cascade,
-sch_seller varchar2(15) not null references member(member_id) on delete cascade,
-sch_rsv_date date default sysdate
+drop table sch cascade constraints purge;
+
+create table sch(
+sch_id varchar2(100) not null primary key, --구분을 위해
+sch_title varchar2(100), --스케줄 제목
+sch_allday varchar2(15), --하루종일인지 아닌지 true/false
+sch_start varchar2(100), --시작 시간
+sch_end varchar2(100), --끝나는 시간
+sch_seller varchar2(100) --판매자id
 )
